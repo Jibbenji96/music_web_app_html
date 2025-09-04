06339e8fbae1dd5a4d4487161998adf9399b418e
Ben Williams <nebjobmail@gmail.com>
2025-09-04 16:36:00 +0100

    Updating attributes to pass test

--- tests/test_artist.py
+++ tests/test_artist.py
@@ -1,18 +1,19 @@
 from lib.artist import Artist
 
 def test_artist_constructs():
-    artist = Artist('Wild nothing', 'Indie')
+    artist = Artist(1, 'Wild nothing', 'Indie')
 
+    assert artist.id == 1
     assert artist.name == 'Wild nothing'
     assert artist.genre == 'Indie'
 
 def test_artist_contents():
-    artist = Artist('Wild nothing', 'Indie')
-    artist1 = Artist('Wild nothing', 'Indie')
+    artist = Artist(1, 'Wild nothing', 'Indie')
+    artist1 = Artist(1, 'Wild nothing', 'Indie')
 
     assert artist == artist1
 
 def test_artist_formats():
-    artist = Artist('Wild nothing', 'Indie')
+    artist = Artist(1, 'Wild nothing', 'Indie')
 
-    assert str(artist) == 'Artist(Wild nothing, Indie)'
+    assert str(artist) == 'Artist(1, Wild nothing, Indie)'
